18e2e486672416506ac2786f78bf822fc1aaedd1
Urimoto-Takaaki <69062224+Urimoto-Takaaki@users.noreply.github.com>
2020-08-06 17:26:57 +0900

    RailsQ-1回答20200806

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User
+  def initialize
+    @f_name = "Urimoto"
+    @l_name = "Takaaki"
+    @age = 32
+    @birth = "1987/10/21"
+    @birth_place = "HyogoHimeji"
+    @myhobby = "Shogi"
+  end
+
+def introduce
+  <<~EOS
+    私の名前は#{@f_name} #{@l_name}です
+    誕生日は#{@birth}で、#{@age}歳です
+    出身地は#{@birth_place}で、趣味は#{@myhobby}です
+
+  EOS
+
+end
+
+
+end
